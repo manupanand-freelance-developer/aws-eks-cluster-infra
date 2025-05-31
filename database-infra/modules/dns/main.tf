8ba05a2888cff8cf5f3eb060e1025467abc66254
manupanand <manupanandindianinvestor@gmail.com>
2025-05-31 23:07:16 +0530

    bug fix

--- database-infra/modules/dns/main.tf
+++ database-infra/modules/dns/main.tf
@@ -5,7 +5,7 @@
 
 
 resource "aws_route53_record" "dns_private" {
-  depends_on = [ aws_route53_zone.private_db_dns ]
+  
   for_each=var.private_ip 
   zone_id = var.zone_id
   name    = "${var.name}.${each.key}"
