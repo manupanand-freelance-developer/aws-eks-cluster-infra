7ef07d83bd7e27e65b13ef67a611c238d80e63a4
manupanand <manupanandindianinvestor@gmail.com>
2025-06-05 11:36:18 +0530

    updated code bug

--- database-infra-default/main.tf
+++ database-infra-default/main.tf
@@ -39,7 +39,7 @@ module "ec2-instance" {
 # route 53
 module "dns" {
 
-  depends_on            = [ module.ec2-instance ,module.security_group,module.dns_zone]
+  depends_on            = [ module.ec2-instance ,module.security_group,]
   source                = "./modules/dns"
   for_each              = var.ec2_instance 
   name                  = each.key
