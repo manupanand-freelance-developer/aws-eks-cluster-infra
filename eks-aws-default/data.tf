ce3159cdea484a80375e1459549b77825b0c0cb8
manupanand <manupanandindianinvestor@gmail.com>
2025-06-27 12:01:55 +0530

    updated code

--- eks-aws-default/data.tf
+++ eks-aws-default/data.tf
@@ -1,10 +1,10 @@
-data "aws_autoscaling_groups" "eks_asgs" {}
-data "aws_autoscaling_instances" "eks_nodes" {}
+# data "aws_autoscaling_groups" "eks_asgs" {}
+# data "aws_autoscaling_instances" "eks_nodes" {}
 
-data "aws_instances" "eks_nodes" {
-    depends_on = [ module.eks]
-  filter {
-    name   = "tag:eks:nodegroup-name"
-    values = module.eks.node_group_names
-  }
-}
+# data "aws_instances" "eks_nodes" {
+#     depends_on = [ module.eks]
+#   filter {
+#     name   = "tag:eks:nodegroup-name"
+#     values = module.eks.node_group_names
+#   }
+# }
